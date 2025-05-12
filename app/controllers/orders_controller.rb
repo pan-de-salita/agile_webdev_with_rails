4fa5177c6f908d6a5225bed732b0f6124948eb78
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-05-13 00:36:42 +0800

    remove unwanted puts call

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,8 +25,6 @@ class OrdersController < ApplicationController
 
   # POST /orders or /orders.json
   def create
-    puts @order
-
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
 
