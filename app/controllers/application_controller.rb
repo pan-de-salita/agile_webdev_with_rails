1f8376e8d6f11f09da9716186636d842cc639700
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2025-06-04 21:28:54 +0800

    feat: iteration j2 | selecting the locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :set_i18n_locale_from_params
+
+  protected
+
+  def set_i18n_locale_from_params
+    return unless params[:locale]
+
+    if I18n.available_locales.map(&:to_s).include?(params[:locale])
+      I18n.locale = params[:locale]
+    else
+      flash.now[:notice] = "#{params[:locale]} translation not available"
+      logger.error flash.now[:notice]
+    end
+  end
 
   private
 
